1697bcd65f298d98cfdd6d11e7e968357c317477
Jakub Orzeł <kuba.orzel@hotmail.com>
2023-10-16 17:53:48 +0200

    Added mobile responsiveness to 'Co robie' section

--- src/components/HeaderContent/Card.jsx
+++ src/components/HeaderContent/Card.jsx
@@ -1,18 +1,47 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styles from './Card.module.css'
 
-const Card = ({imageName, titleText, subtitleText, altText}) => {
+
+
+const Card = ({ imageName, titleText, subtitleText, altText }) => {
+    const primaryImgSize = 255
+    const mobileImgSize = 220
+    const mobilPageWidthPx = 740
+
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= mobilPageWidthPx ? true : false)
+
+    useEffect(() => {
+        function handleWindowResize() {
+            if (window.innerWidth <= mobilPageWidthPx) {
+                setIsMobile(true)
+            } else {
+                setIsMobile(false)
+            }
+        }
+        window.addEventListener('resize', handleWindowResize);
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        };
+    }, []);
+
 
     return (
         <div className={styles.card}>
             <div className={styles.image}>
-                <img src={`card-${imageName}.png`} alt={altText} />
+                <img width={isMobile ? mobileImgSize : primaryImgSize}
+                    height={isMobile ? mobileImgSize : primaryImgSize}
+                    src={`card-${imageName}.png`}
+                    alt={altText} />
             </div>
             <div>
-                <div className={styles.title}>
+                <div
+                    style={isMobile ? {fontSize: 14} : {fontSize: 16}}
+                    className={styles.title}>
                     {titleText}
                 </div>
-                <div className={styles.subtitle}>
+                <div 
+                    style={isMobile ? {fontSize: 12} : {fontSize: 14}}
+                    className={styles.subtitle}>
                     {subtitleText}
                 </div>
             </div>

9efd84fe8879351de58a86b906416443b53d5807
Jakub Orzeł <kuba.orzel@hotmail.com>
2023-10-16 16:20:30 +0200

    Modified contact form

--- src/components/BottomBanner/BottomBanner.jsx
+++ src/components/BottomBanner/BottomBanner.jsx
@@ -1,23 +1,27 @@
 import React from 'react'
 import styles from './BottomBanner.module.css'
-
+import BottomContactForm from '../BottomContactForm/BottomContactForm'
 import bottomBanner from '../../assets/bottomBanner.png'
 import bottomImage from '../../assets/bottomImage.svg'
 
 const BottomBanner = ({ bottomBannerRef }) => {
     return (
         <div>
+
+            <img
+                className={styles.contactimg}
+                src={bottomImage} alt="img" />
             <div
                 ref={bottomBannerRef}
                 style={{ backgroundImage: `url(${bottomBanner})` }}
                 className={styles.banner}>
                 <div
                     className={styles.headertext} >
+                    <div>
+                        <BottomContactForm />
+                    </div>
                 </div>
             </div>
-            <img
-                className={styles.contactimg}
-                src={bottomImage} alt="img" />
         </div>
     )
 }
---
+++ src/components/BottomContactForm/BottomContactForm.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import styles from './BottomContactForm.module.css'
+
+
+const BottomContactForm = ({ }) => {
+    return (
+        <div>
+            <div
+                className={styles.container}>
+                <div
+                    className={`banner-text ${styles.htext}`}>
+                    // Porozmawiajmy!
+                </div>
+                <form>
+                    <div className={styles.text}>
+                        <label for='name'>Imię i nazwisko*</label>
+                        <input id='name' type='text' placeholder='Jan Kowlaski' />
+                    </div>
+                    <div className={styles.text}>
+                        <label for='phone-number'>Telefon*</label>
+                        <input id='phone-number' type='tel' pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" placeholder='600 300 100' />
+                    </div>
+                    <div className={styles.text}>
+                        <label for='message'>Wiadomość (opcjonalnie)</label>
+                        <textarea id='message' placeholder='Napisz kilka zdań o twojej firmie, stronie czy reklamach..' />
+                    </div>
+                    <button
+                        type='submit'
+                        className={styles.button}
+                        >Wyślij
+                    </button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default BottomContactForm

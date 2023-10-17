858ec86748f5d0205e29359ca9441b2cc41d3a23
Jakub Orzeł <kuba.orzel@hotmail.com>
2023-10-17 15:02:48 +0200

    Added mobile nav menu

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,7 @@ function App() {
   return (
     <div>
       <Navbar
+        isMobile={isMobile}
         headerContentRef={headerContentRef}
         middleBannerRef={middleBannerRef}
         bottomBannerRef={bottomBannerRef} />
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,53 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Navbar.module.css'
 
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 import Logo from '../../assets/logo.svg'
 
-const Navbar = ({ middleBannerRef, bottomBannerRef, headerContentRef }) => {
+const Navbar = ({ isMobile, middleBannerRef, bottomBannerRef, headerContentRef }) => {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
     return (
         <header className={styles.navbar}>
-            <img src={Logo} alt="EO" />
-            <nav>
-                <ul className={styles.menu}>
-                    <li>
-                        <a href='/'>Start</a>
-                    </li>
-                    <li>
-                        <a
-                            onClick={() => headerContentRef.current.scrollIntoView({
-                                behavior: "smooth",
-                                inline: "start"
-                            })}>
-                            Co robie
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            onClick={() => middleBannerRef.current.scrollIntoView({
-                                behavior: "smooth",
-                                inline: "start"
-                            })}>
-                            Potrzeby
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            onClick={() => bottomBannerRef.current.scrollIntoView({
-                                behavior: "smooth",
-                                inline: "start"
-                            })}>
-                            Porozmawiajmy
-                        </a>
-                    </li>
-                    <li>
-                        <a href='/'>O mnie</a>
-                    </li>
-                </ul>
-                <div className={styles.menu}>
-                    <AiOutlineMenu />
-                </div>
-            </nav>
+            <div className={styles.upper_bar}>
+                <img className={styles.logo} src={Logo} alt="EO" />
+                {isMobile
+                    ? !mobileMenuOpen
+                        ? <div className={styles.menu_btn}>
+                            <AiOutlineMenu
+                                onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+                                size={25}
+                                cursor={'pointer'} />
+                        </div>
+                        : <div className={styles.menu_btn}>
+                            <AiOutlineClose
+                                onClick={() => setMobileMenuOpen(false)}
+                                size={25}
+                                cursor={'pointer'} />
+                        </div>
+                    : ""}
+            </div>
+            {!isMobile || isMobile && mobileMenuOpen
+                ? <nav>
+                    <ul className={styles.menu}>
+                        <li>
+                            <a
+                                onClick={() => headerContentRef.current.scrollIntoView({
+                                    behavior: "smooth",
+                                    inline: "start"
+                                })}>
+                                Co robie
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                onClick={() => middleBannerRef.current.scrollIntoView({
+                                    behavior: "smooth",
+                                    inline: "start"
+                                })}>
+                                Potrzeby
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                onClick={() => bottomBannerRef.current.scrollIntoView({
+                                    behavior: "smooth",
+                                    inline: "start"
+                                })}>
+                                Porozmawiajmy
+                            </a>
+                        </li>
+                        <li>
+                            <a href='/'>O mnie</a>
+                        </li>
+                    </ul>
+                </nav>
+                : ""}
         </header>
     )
 }

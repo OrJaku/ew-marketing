0e954c7234ee2b0939a16916802e76bb421ac6dd
Jakub Orzeł <kuba.orzel@hotmail.com>
2023-10-18 16:19:25 +0200

    Added phoneNumberFormater

--- src/components/BottomContactForm/BottomContactForm.jsx
+++ src/components/BottomContactForm/BottomContactForm.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './BottomContactForm.module.css'
 
 
+
 const BottomContactForm = ({ }) => {
+    const [name, setName] = useState("")
+    const [phoneNumber, setPhoneNumber] = useState("")
+
+    const [message, setMessage] = useState("")
+
+    const phoneNumberFormatter = (inputValue) => {
+        const number = inputValue.trim().replace(/[^0-9]/g, "");
+        if (number.length < 4) return number;
+        if (number.length < 7) return number.replace(/(\d{3})(\d{1})/, "$1-$2");
+        if (number.length < 11) return number.replace(/(\d{3})(\d{3})(\d{1})/, "$1-$2-$3");
+        return number.replace(/(\d{3})(\d{4})(\d{4})/, "$1-$2-$3");
+    }
+
+    const handlePhoneNumber = (inputValue) => {
+        const number = phoneNumberFormatter(inputValue)
+        setPhoneNumber(number)
+    }
+
     return (
         <div>
             <div
@@ -13,21 +32,37 @@ const BottomContactForm = ({ }) => {
                 </div>
                 <form>
                     <div className={styles.text}>
-                        <label htmlFor='name'>Imię i nazwisko*</label>
-                        <input id='name' type='text' placeholder='Jan Kowlaski' />
+                        <label htmlFor='name'>Imię*</label>
+                        <input
+                            id='name'
+                            type='text'
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            placeholder='Gosia' />
                     </div>
                     <div className={styles.text}>
                         <label htmlFor='phone-number'>Telefon*</label>
-                        <input id='phone-number' type='tel' pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" placeholder='600 300 100' />
+                        <input
+                            id='phone-number'
+                            type='tel'
+                            value={phoneNumber}
+                            onChange={(e) => handlePhoneNumber(e.target.value)}
+                            pattern="[0-9]{3}-[0-9]{3}-[0-9]{3}"
+                            maxLength={11}
+                            placeholder='600-300-100' />
                     </div>
                     <div className={styles.text}>
                         <label htmlFor='message'>Wiadomość (opcjonalnie)</label>
-                        <textarea id='message' placeholder='Napisz kilka zdań o twojej firmie, stronie czy reklamach..' />
+                        <textarea
+                            id='message'
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
+                            placeholder='Napisz kilka zdań o twojej firmie, stronie czy reklamach..' />
                     </div>
                     <button
                         type='submit'
                         className={styles.button}
-                        >Wyślij
+                    >Wyślij
                     </button>
                 </form>
             </div>

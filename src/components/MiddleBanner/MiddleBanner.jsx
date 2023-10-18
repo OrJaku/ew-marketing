79f9a5b5c25092b42e267ffef31428be322cacb6
Jakub Orzeł <kuba.orzel@hotmail.com>
2023-10-18 15:04:38 +0200

    Added base Quiz component

--- src/components/MiddleBanner/MiddleBanner.jsx
+++ src/components/MiddleBanner/MiddleBanner.jsx
@@ -1,26 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './MiddleBanner.module.css'
+import Quiz from './Quiz'
 
 import middleBanner from '../../assets/middleBanner.png'
 import imagePeople from '../../assets/middleImage.svg'
 
 const MiddleBanner = ({ middleBannerRef }) => {
+    const [quizIsOpen, setQuizIsOpen] = useState(true)
     return (
         <div className={styles.container}>
             <div
-                
+
                 ref={middleBannerRef}
                 style={{ backgroundImage: `url(${middleBanner})` }}
                 className={styles.banner}>
                 <div
                     className={`banner-text ${styles.htext}`}>
-                // SPRAWDŹ, CZEGO POTRZEBUJE TWÓJ BIZNES
+                    <div>
+                    // SPRAWDŹ, CZEGO POTRZEBUJE TWÓJ BIZNES
+                    </div>
+                    <div className={styles.description}>
+                        <div className={styles.ltext}>
+                            Odpowiedz na kilka pytań i otrzymaj darmową poradę
+                        </div>
+                        <button
+                            onClick={() => setQuizIsOpen(!quizIsOpen)}
+                            type='button'
+                            className={styles.button}
+                            >{quizIsOpen? "Zamknij": "Zaczynamy!"}
+                        </button>
+                    </div>
                 </div>
 
             </div>
-            <img
+            {/* <img
                 className={styles.peopleimg}
-                src={imagePeople} alt="img" />
+                src={imagePeople} alt="img" /> */}
+            <div className={quizIsOpen? [styles.quiz_container ,styles.qc_active].join(' '): styles.quiz_container}>
+                <Quiz />
+            </div>
         </div>
     )
 }
---
+++ src/components/MiddleBanner/Quiz.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import styles from './Quiz.module.css'
+
+
+const Quiz = () => {
+    return (
+        <div className={styles.questions}>
+            <div>Pytanie 1</div>
+            <div>Pytanie 2</div>
+            <div>Pytanie 3</div>
+            <div>Pytanie 4</div>
+
+        </div>
+    )
+}
+
+export default Quiz

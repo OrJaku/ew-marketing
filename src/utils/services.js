fefb621c1728ea1b6dfb0718e9b75b289c77012b
Jakub Orzeł <kuba.orzel@hotmail.com>
2023-10-19 13:56:23 +0200

    Added try-catch to API service

--- src/utils/services.js
+++ src/utils/services.js
@@ -1,18 +1,23 @@
 export async function sendFormData(url, data = {}, respFunction = null) {
-    const response = await fetch(url, {
-        method: "POST",
-        mode: "cors",
-        cache: "no-cache",
-        credentials: "same-origin",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        redirect: "follow",
-        referrerPolicy: "no-referrer",
-        body: JSON.stringify(data),
-    });
-    if (respFunction !== null) {
-        respFunction(false)
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            mode: "cors",
+            cache: "no-cache",
+            credentials: "same-origin",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            redirect: "follow",
+            referrerPolicy: "no-referrer",
+            body: JSON.stringify(data),
+        });
+        if (respFunction !== null) {
+            respFunction(false)
+        }
+        return response.json();
     }
-    return response.json();
+    catch {
+        console.error("Server error. No API response or incorrect response.")
+    }
 }

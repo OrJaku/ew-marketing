f33966143ec4bb3e41f1c26c80eea293fa731443
Jakub Orzeł <kuba.orzel@hotmail.com>
2023-10-19 16:35:33 +0200

    Added quiz questions

--- src/components/MiddleBanner/Quiz.jsx
+++ src/components/MiddleBanner/Quiz.jsx
@@ -6,11 +6,15 @@ import styles from './Quiz.module.css'
 const Quiz = () => {
     return (
         <div className={styles.questions}>
-            <div>Pytanie 1</div>
-            <div>Pytanie 2</div>
-            <div>Pytanie 3</div>
-            <div>Pytanie 4</div>
-
+            <div>Nie mam reklam, bo nie wiem jak je ustawić</div>
+            <div>Korzystałem z reklam w internecie, ale są zbyt drogie</div>
+            <div>Moje reklamy nie działają</div>
+            <div>Nikt nie wchodzi na moją stronę</div>
+            <div>Chciałabym mieć swoją stronę www, ale nie wiem jak ją zrobić</div>
+            <div>Mam stronę www, ale nie jestem z niej zadowolona/y</div>
+            <div>Nie mam pomysłu na promocję w internecie</div>
+            <div>Moje posty w social mediach są nieskuteczne</div>
+            <div>Nie wiem, jakie treści zainteresowałyby moich klientów</div>
         </div>
     )
 }

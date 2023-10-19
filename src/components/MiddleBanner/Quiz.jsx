9aba141183721fb4c4461cfe397d92e41d28ccd3
Jakub Orzeł <kuba.orzel@hotmail.com>
2023-10-19 21:38:08 +0200

    Modified Quiz questions component logic

--- src/components/MiddleBanner/Quiz.jsx
+++ src/components/MiddleBanner/Quiz.jsx
@@ -1,20 +1,33 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 import styles from './Quiz.module.css'
 
+import questions from '../../utils/quiz_questions.json'
 
 const Quiz = () => {
+
+    const [selectedIds, setSelectedIds] = useState([])
+
+    const handleAddToSelected = (id) => {
+        if (selectedIds.includes(id)) {
+            setSelectedIds(selectedIds.filter(selectedId => selectedId !== id))
+        } else {
+            setSelectedIds(oldIds => [...oldIds, id])
+        }
+    }
+
     return (
         <div className={styles.questions}>
-            <div>Nie mam reklam, bo nie wiem jak je ustawić</div>
-            <div>Korzystałem z reklam w internecie, ale są zbyt drogie</div>
-            <div>Moje reklamy nie działają</div>
-            <div>Nikt nie wchodzi na moją stronę</div>
-            <div>Chciałabym mieć swoją stronę www, ale nie wiem jak ją zrobić</div>
-            <div>Mam stronę www, ale nie jestem z niej zadowolona/y</div>
-            <div>Nie mam pomysłu na promocję w internecie</div>
-            <div>Moje posty w social mediach są nieskuteczne</div>
-            <div>Nie wiem, jakie treści zainteresowałyby moich klientów</div>
+            {Object.entries(questions).map(([id_, val]) => (
+                <div
+                    key={id_}
+                    id={id_}
+                    style={{
+                        backgroundColor: `rgba(21, 173, 244, ${selectedIds.includes(id_) ? 0.88 : 0.48})`
+                    }}
+                    onClick={(e) => handleAddToSelected(e.target.id)}>
+                    {val.question}
+                </div>
+            ))}
         </div>
     )
 }

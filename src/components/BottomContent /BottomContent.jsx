7d96790a50553faa3663179b0ca97245343c4d2d
Jakub Orzeł <kuba.orzel@hotmail.com>
2023-10-26 13:58:01 +0200

    Fixed problem with BottomContent component

--- src/components/BottomContent /BottomContent.jsx
+++ src/components/BottomContent /BottomContent.jsx
@@ -5,25 +5,26 @@ import portretImages from '../../assets/portret.svg'
 
 const BottomContent = ({ isMobile, bottomContentRef }) => {
     return (
-        <div
-            ref={bottomContentRef}
-            className={styles.content}>
-            <div className={styles.portretimage}>
-                <img
-                    src={portretImages} alt="img" />
-            </div>
+        <>
             <div
-                className={`${styles.htext}`}>
-                <div className={`banner-text`}><span className={'double-slash'}>//</span> O MNIE</div>
-                <div className={`${styles.subtitle}`}>EWELINA ORZEŁ</div>
-                <div className={`${styles.description}`}>
-                    <div>Zajmuję się marketingiem internetowym od 8 lat.</div>
-                    <div>Lubię rozwiązywać realne problemy biznesowe korzystając z nowoczesnych narzędzi i swojego doświadczenia.</div>
-                    <div>Siła działań online tkwi w liczbach,  które możemy zbierać, analizować  i wykorzystywać do osiągania  jeszcze lepszych wyników.</div>
+                ref={bottomContentRef}
+                className={styles.content}>
+                <div className={styles.portretimage}>
+                    <img src={portretImages} alt="img" />
+                </div>
+                <div
+                    className={`${styles.htext}`}>
+                    <div className={`banner-text`}><span className={'double-slash'}>//</span> O MNIE</div>
+                    <div className={`${styles.subtitle}`}>EWELINA ORZEŁ</div>
+                    <div className={`${styles.description}`}>
+                        <div>Zajmuję się marketingiem internetowym od 8 lat.</div>
+                        <div>Lubię rozwiązywać realne problemy biznesowe korzystając z nowoczesnych narzędzi i swojego doświadczenia.</div>
+                        <div>Siła działań online tkwi w liczbach,  które możemy zbierać, analizować i wykorzystywać do osiągania jeszcze lepszych wyników.</div>
                     </div>
+                </div>
             </div>
-                            </div>
+        </>
     )
 }
 
-export default BottomContent 
+export default BottomContent

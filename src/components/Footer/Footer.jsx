2d53fd6f431fc99c3ccf7fe55c0ca3f16c920eb4
Jakub Orzeł <kuba.orzel@hotmail.com>
2023-10-26 08:39:12 +0200

    Modified Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,24 +1,22 @@
 import React from 'react'
 import styles from './Footer.module.css'
 
+import contactInfo from '../../utils/contact.json'
 
 const Footer = () => {
     return (
         <div
             className={styles.footer}>
             <div className={styles.content}>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
                 <div className={styles.contact}>
                     <div>Kontakt:</div>
                     <div>
-                        <div>tel: 500-100-100</div>
-                        <div>email: ewelka@gmail.com</div>
-
+                        <div>Telefon: {contactInfo.telefon}</div>
+                        <div>E-mail: {contactInfo.email}</div>
                     </div>
+                </div>
+                <div className={styles.rodo}>
+                    <div>Polityka RODO</div>
                 </div>
             </div>
         </div>

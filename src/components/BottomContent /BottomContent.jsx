c4f3dca4291125aee648431bd0670247947fe437
Jakub Orzeł <kuba.orzel@hotmail.com>
2023-10-26 22:50:08 +0200

    Fixed text wrapper in BottomContent

--- src/components/BottomContent /BottomContent.jsx
+++ src/components/BottomContent /BottomContent.jsx
@@ -17,7 +17,7 @@ const BottomContent = ({ isMobile, bottomContentRef }) => {
                     <div className={`banner-text`}><span className={'double-slash'}>//</span> O MNIE</div>
                     <div className={`${styles.subtitle}`}>EWELINA ORZEŁ</div>
                     <div className={`${styles.description}`}>
-                        <div>Zajmuję się marketingiem internetowym od 8 lat.</div>
+                        <div ><div style={{display: 'inline-block', marginRight: '4px'}}>Zajmuję się marketingiem </div><div style={{display: 'inline-block'}}>internetowym od 8 lat.</div></div>
                         <div>Lubię rozwiązywać realne problemy biznesowe korzystając z nowoczesnych narzędzi i swojego doświadczenia.</div>
                         <div>Siła działań online tkwi w liczbach,  które możemy zbierać, analizować i wykorzystywać do osiągania jeszcze lepszych wyników.</div>
                     </div>
